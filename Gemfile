4a6d29f6e4eed2b61397e4a3c093f44faaeaddfb
Stasyanx <stanislav@inservery.com>
2014-06-08 12:14:15 +0300

    devise user

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://rubygems.org'
+gem 'rails', '4.0.4'
+
+gem 'sass-rails', '~> 4.0.2'
+
+gem 'uglifier', '>= 1.3.0'
+
+gem 'coffee-rails', '~> 4.0.0'
+gem 'cancan'
+
+gem 'jquery-rails'
+
+gem 'turbolinks'
+
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+gem 'puma'
+gem 'pg'
+gem 'devise'
+
+gem 'bcrypt', '~> 3.1.7'
+gem 'sqlite3'
+
+
+gem 'debugger', group: [:development, :test]
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '5ff6b43b6f3647e4e3d21c0243793cc49ee4af9bde1859c4d175a793bac8a3e06d57bebf09c9b37d3ed099cf5e3ca7d5a9f8f82ec19d976b45848d425b09095e'
