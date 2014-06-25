dd1bd860509fceddfbff6e3a919312857a21e1f6
Stasyanx <stanislav@inservery.com>
2014-06-25 16:54:25 +0300

    cleaning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :set_il8n_locale_from_params
 
-  def current_ability
-    @current_ability ||= Ability.new(current_user)
-  end
 
   protected
 
